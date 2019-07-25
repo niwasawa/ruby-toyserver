a704918811a7c412aeb2592a36bdb852352b3c4d
Naoki Iwasawa <niwasawa@maigo.info>
2019-07-25 21:21:57 +0900

    Change to a class ToyServer

--- toyserver.rb
+++ toyserver.rb
@@ -2,55 +2,64 @@ require 'webrick'
 require 'cgi'
 require 'json'
 
-server = WEBrick::HTTPServer.new(
-  :Port => 8000,
-  :HTTPVersion => WEBrick::HTTPVersion.new('1.1'),
-  :AccessLog => [[open(IO::NULL, 'w'), '']] # don't output access log
-)
+class ToyServer
 
-server.mount_proc('/') do |req, res|
+  def start()
 
-  # output request infomation
-  puts "========== #{Time.new} =========="
-  puts req.request_line
-  puts ''
-  puts req.raw_header
-  puts "\n#{req.body}" if req.body
+    server = WEBrick::HTTPServer.new(
+      :Port => 8000,
+      :HTTPVersion => WEBrick::HTTPVersion.new('1.1'),
+      :AccessLog => [[open(IO::NULL, 'w'), '']] # don't output access log
+    )
 
-  # status
-  status = 200
-  if req.query.include?('status')
-    status = req.query['status'].to_i
-  end
+    server.mount_proc('/') do |req, res|
 
-  # headers
-  headers = res.header
-  headers['content-type'] = 'text/html'
-  if req.query.include?('headers')
-    JSON.parse(req.query['headers']).each do |key, val|
-      headers[key] = val
+      # output request infomation
+      puts "========== #{Time.new} =========="
+      puts req.request_line
+      puts ''
+      puts req.raw_header
+      puts "\n#{req.body}" if req.body
+
+      # status
+      status = 200
+      if req.query.include?('status')
+        status = req.query['status'].to_i
+      end
+
+      # headers
+      headers = res.header
+      headers['content-type'] = 'text/html'
+      if req.query.include?('headers')
+        JSON.parse(req.query['headers']).each do |key, val|
+          headers[key] = val
+        end
+      end
+
+      # response body
+      body = "<html><body>\n"
+      body += "#{CGI.escapeHTML(req.request_line)}"
+      body += "<br>#{CGI.escapeHTML(req.body)}\n" if req.body
+      body += "</body></html>\n"
+      if req.query.include?('body')
+        body = req.query['body']
+      end
+
+      # wait
+      if req.query.include?('wait')
+        sleep(req.query['wait'].to_i)
+      end
+
+      # return response
+      res.status = status
+      res.body = body
     end
+
+    Signal.trap('INT'){server.shutdown}
+    server.start
   end
 
-  # response body
-  body = "<html><body>\n"
-  body += "#{CGI.escapeHTML(req.request_line)}"
-  body += "<br>#{CGI.escapeHTML(req.body)}\n" if req.body
-  body += "</body></html>\n"
-  if req.query.include?('body')
-    body = req.query['body']
-  end
-
-  # wait
-  if req.query.include?('wait')
-    sleep(req.query['wait'].to_i)
-  end
-
-  # return response
-  res.status = status
-  res.body = body
 end
 
-Signal.trap('INT'){server.shutdown}
-server.start
+ToyServer.new.start
 
